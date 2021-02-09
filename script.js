a3a1c40d66d6b407dc99cafe03367845ea7d5f07
mkmtd <77616280+mkmtd@users.noreply.github.com>
2021-02-09 21:21:24 +0900

    changeJS
    JavaScriptファイルの記述の変更。

--- script.js
+++ script.js
@@ -13,18 +13,9 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   //Q&Aドロップダウン（仮コード）
-  document.getElementById("question_01").addEventListener("click", function () {
+  document.getElementsByClassName("question").addEventListener("click", function () {
     this.classList.toggle("active");
-    document.getElementById("answer_01").classList.toggle("active");
+    document.getElementsByClassName("answer").classList.toggle("active");
   });
 
-  document.getElementById("question_02").addEventListener("click", function () {
-    this.classList.toggle("active");
-    document.getElementById("answer_02").classList.toggle("active");
-  });
-
-  document.getElementById("question_03").addEventListener("click", function () {
-    this.classList.toggle("active");
-    document.getElementById("answer_03").classList.toggle("active");
-  });
 });
