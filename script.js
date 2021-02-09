d1a987d884425170b7a15c2b6728a04c27f7444d
mkmtd <77616280+mkmtd@users.noreply.github.com>
2021-02-09 21:28:49 +0900

    changeJsAgain
    変更コードでエラーが出たため、再度変更。

--- script.js
+++ script.js
@@ -13,9 +13,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   //Q&Aドロップダウン（仮コード）
-  document.getElementsByClassName("question").addEventListener("click", function () {
+  document.getElementsById("question_01").addEventListener("click", function () {
     this.classList.toggle("active");
-    document.getElementsByClassName("answer").classList.toggle("active");
+    document.getElementsById("answer_01").classList.toggle("active");
+  });
+  
+  
+  document.getElementsById("question_02").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.getElementsById("answer_02").classList.toggle("active");
+  });
+  
+  document.getElementsById("question_03").addEventListener("click", function () {
+    this.classList.toggle("active");
+    document.getElementsById("answer_03").classList.toggle("active");
   });
 
 });
